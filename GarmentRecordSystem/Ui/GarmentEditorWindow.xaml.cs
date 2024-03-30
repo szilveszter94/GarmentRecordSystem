0cd52e0a7b6ffadd9019dbf5ac0a884bdc2e1f72
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-30 22:57:06 +0100

    Add new window boilerplate for edit and create garments

---
+++ GarmentRecordSystem/Ui/GarmentEditorWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace GarmentRecordSystem.Ui;
+
+public partial class GarmentEditorWindow : Window
+{
+    public GarmentEditorWindow()
+    {
+        InitializeComponent();
+    }
+}

ef072dbb51da3b3bb64be06cdc7ce3a53b863d1a
Szilveszter <s.szilveszter1994@gmail.com>
2024-04-01 01:20:53 +0200

    Refactor the garment repository

--- GarmentRecordSystem/Repository/IGarmentRepository.cs
+++ GarmentRecordSystem/Repository/IGarmentRepository.cs
@@ -6,9 +6,10 @@ namespace GarmentRecordSystem.Repository;
 public interface IGarmentRepository
 {
     IEnumerable<GarmentModel> GetAllGarments();
+    bool LoadGarmentsAndSetNewFilePath(string filePath);
     GarmentModel GetGarmentById(int garmentId);
     GarmentModel AddGarment(GarmentModel garment);
     GarmentModel UpdateGarment(GarmentModel updatedGarment);
-    void SaveGarments(List<GarmentModel> garments, string? path = null);
+    void SaveGarments(string? path = null);
     void DeleteGarmentById(int garmentId);
 }
